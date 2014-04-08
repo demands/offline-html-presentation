80a93d294e14e624bda1a37b5b3717fe92e23fca
Max Edmands <me@maxedmands.com>
2014-04-08 08:26:41 -0700

    Color-related endpoints

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var PORT = process.env.PORT || 9999;
 
+var _ = require('lodash');
 var express = require('express');
 var fs = require('fs');
 var app = express()
+var colors = require('x11-color-names');
+var colorsByName = _(colors).indexBy('name');
+var colorsByCategory = _(colors).groupBy('category');
 
 app.use(express.logger());
+app.use(express.bodyParser());
 
 app.get(/^\/ex3\//, function(req, res) {
   fs.readFile(__dirname + '/public/awesome-sauce-3.html', {encoding: 'utf8'}, function(err, file) {
@@ -22,6 +27,30 @@ app.get(/^\/ex3ff\//, function(req, res) {
   fs.readFile(__dirname + '/public/awesome-sauce-3-fixed-2.html', {encoding: 'utf8'}, function(err, file) {
     res.send(file);
   });
+});
+
+app.get('/colors', function(req, res) {
+  if(req.query.category) {
+    if(colorsByCategory[req.query.category]) {
+      res.send(colorsByCategory[req.query.category]);
+    } else {
+      res.send(404);
+    }
+  } else {
+    res.send(colors);
+  }
+});
+
+app.get('/colors/:name', function(req, res) {
+  if(colorsByName[req.params.name]) {
+    res.send(colorsByName[req.params.name]);
+  } else {
+    res.send(404);
+  }
+});
+
+app.post('/colors/:name', function(req, res) {
+  console.log(req.body);
 });
 
 app.use(express.static(__dirname + '/public'));
