8e7e653236899ade2ade26c00a2b57ab4f6b5ee3
Max Edmands <me@maxedmands.com>
2014-04-08 12:32:11 -0700

    Add a way to quickly display colors

--- public/angular-setup.js
+++ public/angular-setup.js
@@ -1,6 +1,11 @@
 var app = angular.module('example-app', ['ngResource', 'ngCachedResource']);
 
-app.controller('ExampleController', function($resource, $cachedResource) {
+app.controller('ExampleController', function($scope, $resource, $cachedResource) {
   window.$resource = $resource;
   window.$cachedResource = $cachedResource;
+  window.setColor = function(color) {
+    $scope.color = color;
+    $scope.$digest();
+  };
 });
+
